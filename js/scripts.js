dbb7e8d329750d3451d62138bcd80f2ad5521af5
CryBer <sunkristap@gmail.com>
2021-04-20 15:53:28 +0300

    popUp v.1

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-console.log("JavaScript script is working!")
+
+document.getElementById('addTask').addEventListener('click', () => {
+    console.log('Button was clicked!');
+
+    let popUp = document.getElementById('popUp');
+
+    popUp.style.display = 'block'
+})
